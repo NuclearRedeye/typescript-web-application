640d66201a2ba073597368a8149dcb7a49be530d
NuclearRedeye <github@mneah.com>
2020-09-11 15:09:55 +0100

    chore: directory structure

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { createTextElement } from './utils.js';
+import { createTextElement } from './utils/utils.js';
 
 window.onload = function(): void {
   document.body.appendChild(createTextElement('Created by NuclearRedeye'));
